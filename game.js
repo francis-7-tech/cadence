7b55cacfe2881cbe59fba0e40b89226e6cec9717
Francis <obidigwefrancis94@gmail.com>
2026-08-21 23:24:11 +0100

    Initial Cadence game implementation with HTML, CSS, and JS

---
+++ game.js
@@ -0,0 +1,97 @@
+let gamePattern = [];
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let userClickedPattern = [];
+
+let level = 0;
+let started = false;
+
+$(document).on("keypress", function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+})
+
+function nextSequence(){
+
+  level++;
+
+  $("#level-title").text("Level " + level);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  
+  let randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+  
+}
+
+$(".btn").on("click", function(){
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1); 
+
+    console.log(userClickedPattern);
+});
+
+function playSound(name){
+    let audio = new Audio("sounds/" +  name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed")
+    }, 100);
+    
+}
+
+function checkAnswer(currentLevel){
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function(){
+        nextSequence();
+      }, 1000);
+
+      userClickedPattern = [];
+    }
+  } else {
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("h1").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+    userClickedPattern = [];
+}
+
+
+
+
+
